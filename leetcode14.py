1e18ee101868f3075e1e5a993b5bda7019ee3a96
zhao's ubuntu <2973052533@qq.com>
2020-07-13 14:13:33 +0800

    版本1

---
+++ leetcode14.py
@@ -0,0 +1,12 @@
+'''最长公共前缀'''
+class Solution:
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+        if not strs:
+            return ""
+        s1 = min(strs)
+        s2 = max(strs)
+        for i,x in enumerate(s1):
+            if x != s2[i]:
+                return s2[:i]
+        return s1
+
